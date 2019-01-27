c713934c797b4a5df5b78ead2f514743a5542d6d
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-27 17:26:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
     if board[index] == " " || board[index] == "" || board[index] == nil
      false
-     elsif board
+     elsif board[index] == X 
    
  end
 end
